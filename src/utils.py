88109f2c767f3b48fa192039db1c8ce411fdb34f
Noam Bressler <noam@deepchecks.com>
2021-07-04 23:59:04 +0300

    add ROC and PR curves to logger

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import librosa
 import numpy as np
 import torch
 import soundfile as sf
+import wandb
 from sklearn import metrics
 from unittest.mock import Mock
 import boto3
@@ -136,29 +137,38 @@ class Logger:
         _, predicted = torch.max(pred_tuple[0].data, 1)
         label = pred_tuple[1].data
         self.pred_list += predicted.cpu().numpy().tolist()  # add current batch prediction to full epoch pred list
-        self.pred_proba_list += torch.softmax(pred_tuple[0].data, 1).cpu().numpy()[:, 1].tolist()
+        self.pred_proba_list.append(torch.softmax(pred_tuple[0].data, 1).cpu().numpy())
         self.label_list += label.cpu().numpy().tolist()
 
     def calc_metrics(self, epoch):
         """calculates metrics, saves to tensorboard log & flush prediction list"""
-        self.metrics_dict = self.get_metrics_dict(self.label_list, self.pred_list, self.pred_proba_list)
+        pred_proba_array = np.concatenate(self.pred_proba_list)
+        self.metrics_dict = self.get_metrics_dict(self.label_list, self.pred_list, pred_proba_array)
         self.log_writer.log({'Accuracy': self.metrics_dict['accuracy']}, step=epoch)
         self.log_writer.log({'f1score': self.metrics_dict['f1score']}, step=epoch)
         self.log_writer.log({'precision': self.metrics_dict['precision']}, step=epoch)
         self.log_writer.log({'recall': self.metrics_dict['recall']}, step=epoch)
         self.log_writer.log({'auc': self.metrics_dict['auc']}, step=epoch)
+        self.log_writer.log(
+            {'ROC Curve': wandb.plot.roc_curve(self.label_list, pred_proba_array, labels=['Noise', 'Call'])},
+            step=epoch
+        )
+        self.log_writer.log(
+            {'PR Curve': wandb.plot.pr_curve(self.label_list, pred_proba_array, labels=['Noise', 'Call'])},
+            step=epoch
+        )
         self.pred_list = []  # flush
         self.label_list = []
         self.pred_proba_list = []
 
     @staticmethod
-    def get_metrics_dict(label_list, pred_list, pred_proba_list):
+    def get_metrics_dict(label_list, pred_list, pred_proba_array):
         """calculate the metrics comparing the predictions to the ground-truth labels, and return them in dict format"""
         accuracy = metrics.accuracy_score(label_list, pred_list)  # calculate accuracy using sklearn.metrics
         f1score = metrics.f1_score(label_list, pred_list)
         precision = metrics.precision_score(label_list, pred_list)
         recall = metrics.recall_score(label_list, pred_list)
-        auc = metrics.roc_auc_score(label_list, pred_proba_list)
+        auc = metrics.roc_auc_score(label_list, pred_proba_array[:, 1])
         metrics_dict = {'accuracy': accuracy, 'f1score': f1score, 'precision': precision, 'recall': recall, 'auc': auc}
         return metrics_dict
 
