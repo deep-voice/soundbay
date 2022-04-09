41f3ac87e9a0ed26548af90e65827194ea4ff4c3
z30g0d <zeogod@gmail.com>
2022-04-09 18:13:39 +0300

    initial commit

---
+++ soundbay/utils/multispecies_preprocess.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import soundfile as sf
+from pathlib import Path
+import re
+import os
+import json
+from datetime import datetime,timedelta
+
+
+def df_freq_range_dict(x):
+    return {
+        'LF-200Hz': 'HYDBBA105_Calls_2018_04_200Hz.csv',
+        'LF-2kHz': 'HYDBBA105_Calls_2018_04_2kHz.csv',
+        'MF': 'HYDBBA105_Calls_2018_04_EvenDays_MF.csv'
+
+    }.get(x, None)
+
+def df_attach_wav_files(row):
+    folder_prefix = '2018_04/OOI_105_'
+    file_prefix = 'HYDBBA105OOI_'
+#     date_time_obj = datetime. strptime(row['UTC'], '%Y-%m-%d %H:%M:%S.%f')
+    format_1 = '%m/%d/%Y %H:%M:%S'
+    format_2 = '%m/%d/%Y %H:%M'
+    
+    date_time_obj = datetime. strptime(row['UTC'], '%m/%d/%Y %H:%M:%S')
+    date_time_obj = date_time_obj - timedelta(minutes=date_time_obj.minute % 10,
+                                seconds=date_time_obj.second,
+                                microseconds=date_time_obj.microsecond)
+#     '4/1/2018 12:05:53'
+    begin_time = date_time_obj - timedelta(minutes=(date_time_obj.minute - (date_time_obj.minute % 10)),
+                                seconds=date_time_obj.second,
+                                microseconds=date_time_obj.microsecond)
+    file_address = datetime.strftime(date_time_obj,folder_prefix + '%Y_%m_%d/' + file_prefix +'%Y%m%d-%H%M%S.wav')
+
+    return file_address
+    
+
+def process_row(row, metadata_dict):
+#     print(row['PG_UID'])
+    freq_range_df = df_freq_range_dict(row['Run'])
+    if freq_range_df is None:
+        print('No such df!')
+    else:
+        chosen_freq_df = metadata_dict[freq_range_df]
+        sub_df = chosen_freq_df[chosen_freq_df['parentUID'] == row['PG_UID']]
+        sub_df = sub_df[['UID','UTC','UTCMilliseconds']]
+        sub_df['Species_ID'] = row['Species_ID']
+        sub_df['Sound_type'] = row['Sound_type']
+        sub_df['PG_UID'] = row['PG_UID']
+        sub_df['file'] = df_attach_wav_files(row)
+        
+    
+    return sub_df
+
+def main():
+    print("Hello World!")
+    selection_path = Path('../../../../multispecies_dataset')
+    filelist = list(selection_path.glob('**/*.csv'))
+
+    metadata =[]
+    metadata_dict = {}
+    for i,file in enumerate(filelist):
+        dfTemp = pd.read_csv(file)
+        print(file.name)
+        print('Columns names')
+        print(dfTemp.columns)
+        print('Dataframe shape')
+        print(dfTemp.shape)
+    #     dfTemp['filename'] = re.search(r'18\d{4,4}_\d{6,6}', file.as_posix()).group() 
+    #     dfTemp['StartMicInWater'] =np.amin(dfTemp['Begin Time (s)'])
+    #     dfTemp['EndMicInWater'] =np.amax(dfTemp['End Time (s)'])
+        metadata.append(dfTemp)
+        metadata_dict[file.name] = dfTemp
+    Calls_2018_04_2kHz, Calls_2018_04_EvenDays_MF, Calls_2018_04_200Hz,EvenDays_AllRuns_v2 = metadata
+
+
+    chosen_categories = ['Delph sp.   ', 'Fin whale   ', 'Mn          ']
+    filtered_categories_df = EvenDays_AllRuns_v2[EvenDays_AllRuns_v2['Species_ID'].isin(chosen_categories)]
+    total_df = []
+    for _, row in filtered_categories_df.iterrows():
+    #     print(row)
+        total_df.append(process_row(row, metadata_dict))
+
+    total_df = pd.concat(total_df)
+
+
+if __name__ == "__main__":
+    main()
+
