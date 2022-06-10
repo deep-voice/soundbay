6d1c0a5c07daf38fac3a2b81181c7bdea221bc8e
z30g0d <zeogod@gmail.com>
2022-06-10 18:02:23 +0300

    download recordings from s3

--- soundbay/utils/download_recordings_from_s3.py
+++ soundbay/utils/download_recordings_from_s3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+from pydoc import describe
 import pandas as pd
+import subprocess
 import numpy as np
+from tqdm import tqdm
 import sys
 if __name__=="__main__":
     _, csv_path, download_path = sys.argv
@@ -8,5 +11,9 @@ if __name__=="__main__":
     print(f'size of the dataframe is {df.shape}')
     print(df.columns)
     unique_files, freq = np.unique(df['filename'], return_counts=True)
-    print(unique_files, freq)
+    # print(unique_files, freq)
+    iterator = tqdm(list(unique_files), desc='Downloading files from s3...')
+    for ifile in iterator:
+        run_cmd = f'aws s3 cp {ifile} {download_path} --dryrun'
+        subprocess.run(run_cmd, shell=True)
     print(csv_path, download_path)
