951dec93cae36debf1394dfd9a751554bef1d4ee
z30g0d <zeogod@gmail.com>
2022-09-17 18:50:21 +0300

    source /home/z30g0d/anaconda3/bin/activateRevert "more chnages" conda activate finding_willy_cloned
    This reverts commit db16b4057fe9b5a6f6b3ca2e7752d1f8ca52ea04.

--- soundbay/conf/omegaconf/classes_enums.py
+++ soundbay/conf/omegaconf/classes_enums.py
@@ -1,11 +1,11 @@
 from enum import Enum
 import torch
 
-class scheduler(Enum, str):
+class scheduler(Enum):
     exponent = torch.optim.lr_scheduler.ExponentialLR
     plateau = torch.optim.lr_scheduler.ReduceLROnPlateau
 
 
-class criterion(Enum, str):
+class criterion(Enum):
     cross_entropy = torch.nn.CrossEntropyLoss
     binary_cross_entropy = torch.nn.BCELoss
