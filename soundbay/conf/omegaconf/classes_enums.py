db16b4057fe9b5a6f6b3ca2e7752d1f8ca52ea04
z30g0d <zeogod@gmail.com>
2022-09-17 18:49:02 +0300

    more chnages

--- soundbay/conf/omegaconf/classes_enums.py
+++ soundbay/conf/omegaconf/classes_enums.py
@@ -1,11 +1,11 @@
 from enum import Enum
 import torch
 
-class scheduler(Enum):
+class scheduler(Enum, str):
     exponent = torch.optim.lr_scheduler.ExponentialLR
     plateau = torch.optim.lr_scheduler.ReduceLROnPlateau
 
 
-class criterion(Enum):
+class criterion(Enum, str):
     cross_entropy = torch.nn.CrossEntropyLoss
     binary_cross_entropy = torch.nn.BCELoss
