1f564a1a036cb2ad38f683dc14650172787419ac
z30g0d <zeogod@gmail.com>
2022-10-15 18:51:09 +0300

    introducing changes

--- soundbay/train.py
+++ soundbay/train.py
@@ -21,6 +21,10 @@ from torch.utils.data import DataLoader, WeightedRandomSampler
 import wandb
 from functools import partial
 from pathlib import Path
+from omegaconf import DictConfig, OmegaConf, ListConfig
+
+
+
 import hydra
 from hydra.utils import instantiate
 
@@ -30,7 +34,15 @@ import os
 from soundbay.utils.app import App
 from soundbay.utils.logging import Logger, flatten, get_experiment_name
 from soundbay.utils.checkpoint_utils import upload_experiment_to_s3
+# from soundbay.utils.pydantic_validator import MyConfigSchema
+from hydra.core.config_store import ConfigStore
+from pydantic.dataclasses import dataclass
+from pydantic import validator, BaseModel
+
+
+
 from soundbay.trainers import Trainer
+
 
 
 def modeling(
@@ -67,8 +79,8 @@ def modeling(
 
     """
     # Set paths and create dataset
-    train_dataset = instantiate(train_dataset_args)
-    val_dataset = instantiate(val_dataset_args)
+    train_dataset = instantiate(train_dataset_args,_recursive_=False)
+    val_dataset = instantiate(val_dataset_args, _recursive_=False)
 
     # Define model and device for training
     model = instantiate(model_args)
@@ -125,11 +137,83 @@ def modeling(
 
     return
 
+class Dataset(BaseModel):
+    # label_names: ListConfig
+    batch_size: int
+    num_workers: int
+    sample_rate: int
+    data_sample_rate: int
+    max_freq: int
+    min_freq_filtering: int 
+    n_fft: int
+    hop_length: int
+    train_dataset: dict
+    val_dataset: dict
+    class Config:
+        title = "Dataset"
+        # max_anystr_length = 10
+        allow_mutation = False
+        validate_assignment = True
+        anystr_lower = False
+        validate_all = True
+        use_enum_values = True
+
+    @validator("num_workers")
+    def validate_num_workers(cls, num_workers:int):
+        if num_workers > 4:
+            raise ValueError(f"Number of works is larger than 4 {num_workers}")
+        return num_workers
+
+
+    @validator("train_dataset")
+    def validate_train_dataset(cls, train_dataset:dict) :
+        possible_train_datasets = ['soundbay.data.ClassifierDataset', 'soundbay.data.BaseDataset']
+        if train_dataset['_target_'] not in possible_train_datasets: 
+            raise ValueError(f"Train_dataset is not allowed from type {train_dataset['_target_']}")
+        return train_dataset
+
+class Model(BaseModel):
+    criterion: dict
+    model: dict
+
+    class Config:
+        title = "Model"
+        # max_anystr_length = 10
+        allow_mutation = False
+        validate_assignment = True
+        anystr_lower = True
+        validate_all = True
+        use_enum_values = True
+
+    @validator("criterion")
+    def validate_criterion(cls, criterion:int):
+        # p = Path(path)
+        possible_values = ['torch.nn.MSELoss', 'torch.nn.CrossEntropyLoss']
+        if criterion['_target_'] not in possible_values:
+            raise ValueError(f"'This criterion is not allowed: {criterion['_target_']}")
+        return criterion
+
+
+    @validator("model")
+    def validate_model(cls, model:dict):
+        possible_values = ['models.ResNet1Channel', 'models.GoogleResNet50withPCEN']
+        if model['_target_'] not in possible_values:
+            raise ValueError(f"'This model is not allowed: {model['_target_']}")
+        return model
+
+class Config(BaseModel):
+    data: Dataset
+    model: Model
+
 
 # TODO check how to use hydra without path override
-@hydra.main(config_name="runs/main", config_path="conf")
-def main(args):
-
+@hydra.main(config_name="main", config_path="conf", version_base='1.1')
+def main(validate_args: Config) -> None:
+    
+    args = validate_args.copy()
+    OmegaConf.resolve(validate_args)
+    r_model = Config(**validate_args)
+    # x_model = OmegaConf.to_object(validate_args)
     # Set logger
     _logger = wandb if not args.experiment.debug else Mock()
     experiment_name = get_experiment_name(args)
