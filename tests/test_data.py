79c1a4ad4c3a1bc7cb82d977df4d8a3b937ab455
z30g0d <zeogod@gmail.com>
2022-10-29 14:28:07 +0300

    fix test according to new hydra

--- tests/test_data.py
+++ tests/test_data.py
@@ -12,7 +12,7 @@ def test_dataloader() -> None:
     seed(1)
     with initialize(config_path=os.path.join("..", 'soundbay', 'conf')):
         # config is relative to a module
-        cfg = compose(config_name=os.path.join("runs", "main"))
+        cfg = compose(config_name="main")
         dataset = ClassifierDataset(cfg.data.train_dataset.data_path, cfg.data.train_dataset.metadata_path,
                                         augmentations=cfg.data.train_dataset.augmentations,
                                         augmentations_p=cfg.data.train_dataset.augmentations_p,
