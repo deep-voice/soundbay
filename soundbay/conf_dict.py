66b7bed40f0bab91de391303cf8b4e55d986f09f
deepvoicemachine <info@deepvoicefoundation.com>
2023-02-05 19:31:19 +0000

    enable kwargs intake from conf files

--- soundbay/conf_dict.py
+++ soundbay/conf_dict.py
@@ -3,7 +3,7 @@ Configuration dicts
 -------
 These dicts describe the allowed values of the soundbay framework
 '''
-from soundbay.models import ResNet1Channel, GoogleResNet50withPCEN
+from soundbay.models import ResNet1Channel, GoogleResNet50withPCEN, ChristophCNN
 from soundbay.data import ClassifierDataset, BaseDataset
 import torch
 from audiomentations import PitchShift, BandStopFilter, TimeMask, TimeStretch
@@ -11,7 +11,8 @@ from audiomentations import PitchShift, BandStopFilter, TimeMask, TimeStretch
 
 
 models_dict = {'models.ResNet1Channel':ResNet1Channel,
-'models.GoogleResNet50withPCEN': GoogleResNet50withPCEN}
+'models.GoogleResNet50withPCEN': GoogleResNet50withPCEN,
+'models.ChristophCNN':ChristophCNN}
 
 
 datasets_dict = {'soundbay.data.ClassifierDataset':ClassifierDataset}
