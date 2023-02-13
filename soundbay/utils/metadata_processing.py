94dfd9253f3aa5e53666d028b04d7e77f4a7a0c3
rafaelsul123 <rafaelsul123@gmail.com>
2023-02-13 16:30:36 +0200

    updated notebook + metadata_processing.py to allow a naming dict for annotations corresponding wav files

--- soundbay/utils/metadata_processing.py
+++ soundbay/utils/metadata_processing.py
@@ -10,11 +10,15 @@ from typing import List
 # TODO add tests to the utils in this file
 
 def load_n_adapt_raven_annotation_table_to_dv_dataset_requirements(annotation_file_path: str,
+                                                                   annotation_filename_dict: dict = None,
                                                                    filename_suffix: str = ".Table.1.selections.txt"
                                                                    ) -> pd.DataFrame:
     # todo: decide whether to add annotation treatment
     df_annotations = pd.read_csv(annotation_file_path, sep="\t")
-    df_annotations['filename'] = os.path.basename(annotation_file_path).replace(filename_suffix, '')
+    if annotation_filename_dict is not None:
+        df_annotations['filename'] = annotation_filename_dict[os.path.basename(annotation_file_path)].replace('.txt', '')
+    else:
+        df_annotations['filename'] = os.path.basename(annotation_file_path).replace(filename_suffix, '')
     df_annotations = df_annotations.rename(columns={'Begin Time (s)': 'begin_time', 'End Time (s)': 'end_time'})
     df_annotations['call_length'] = df_annotations['end_time'] - df_annotations['begin_time']
     return df_annotations
@@ -184,3 +188,71 @@ def reset_overlap_accessory_columns(df) -> pd.DataFrame:
 def mark_overlapping_rows(df) -> pd.DataFrame:
     df.loc[df.next_begin_time < df.end_time, 'overlap'] = 1
     return df
+
+
+if __name__ == "__main__":
+    annotations_dir = '../../datasets/2021_annotations/'
+    cols2drop = ['View', 'Channel', 'Low Freq (Hz)', 'High Freq (Hz)', 'Delta Time (s)', 'Delta Freq (Hz)',
+                 'Avg Power Density (dB FS/Hz)']
+
+    # treat annotation naming, duplicates, etc...
+    annotation_filename_dict = {'210825-135601_Tr1.Table.1.selections.txt': '210825-135601_Tr1.wav',
+                                '210825-135601_Tr1.Table.1.selections (1).txt': 'duplicate',
+                                '210904-111316_Tr2.txt': '210904-111316_Tr2.wav',
+                                '210904-111316_Tr2restofrecord.txt': 'corrupt',
+                                '210904-111316_Tr2(first 4 minutes)(1).txt': 'duplicate',
+                                '210904-111316_Tr2(first 4 minutes).txt': 'duplicate',
+                                '210827-133618_Tr2.Table.1.selections.txt': '210827-133618_Tr2.wav',
+                                '210904-093942_Tr2.Table.1.selections.txt': '210904-093942_Tr2.wav',
+                                '210828-080644_Tr1.Table.1.selections.txt': '210828-080644_Tr1.wav',
+                                '210827-081513_Tr1.Table.1.selections.txt': '210827-081513_Tr1.wav',
+                                '210825-132034_Tr1.Table.1.selections.txt': '210825-132034_Tr1.wav',
+                                '210824-104507_Tr1.Table.1.selections.txt': '210824-104507_Tr1.wav',
+                                '210824-104507_Tr1.txt': '210824-104507_Tr1.wav',
+                                '210825-112937_Tr1.txt': '210825-112937_Tr1.wav',
+                                '210904-074321_Tr1.Table.1.selections.txt': '210904-074321_Tr1.wav',
+                                '25-115438_Tr2.Table.1.selections.txt': 'unknown',
+                                '210903-110841_Tr2.Table.1.selections.txt': '210903-110841_Tr2.wav',
+                                '210825-102141_Tr1.txt': '210825-102141_Tr1.wav',
+                                '210903-095104_Tr2.Table.1.selections.txt': '210903-095104_Tr2.wav',
+                                '210903-095104_Tr1.Table.1.selections.txt': '210903-095104_Tr1.wav',
+                                '210825-135601_Tr1.txt': '210825-135601_Tr1.wav',
+                                '210824-125439_Tr1.txt': '210824-125439_Tr1.wav',
+                                '180913_081527 (1).Table.1.selections.txt': 'unknown',
+                                '210824-115331_Tr1.txt': '210824-115331_Tr1.wav',
+                                '210825-112937_Tr2.txt': '210825-112937_Tr2.wav',
+                                '210825-132034_Tr2.Table.1.selections.txt': '210825-132034_Tr2.wav',
+                                '210827-133618_Tr1.Table.1.selections.donetxt.txt': '210827-133618_Tr1.wav',
+                                '210826-083608_Tr1.Table.1.selections.txt': '210826-083608_Tr1.wav',
+                                '210827-081513_Tr2.Table.1.selections.txt': '210827-081513_Tr2.wav',
+                                '210824-100209_Tr1.txt': '210824-100209_Tr1.wav',
+                                '210824-095226_Tr2.txt': '210824-095226_Tr2.wav',
+                                '210827-090209_Tr1.Table.1.selections.txt': '210827-090209_Tr2.wav',
+                                '210903-110841_Tr1.Table.1.selections.txt': '210903-110841_Tr1.wav',
+                                '210824-100209_Tr2.txt': '210824-100209_Tr2.wav',
+                                '210824-095226_Tr1.txt': '210824-095226_Tr1.wav',
+                                '210904-093942_Tr1.Table.1.selections- Annotated.txt': '210904-093942_Tr1.wav',
+                                '210903-095104_Tr1.Table.1.selections.txt': '210903-095104_Tr1.wav'
+                                }
+
+    df_list = []
+    for filename in os.listdir(annotations_dir):
+        try:
+            #         print(filename)
+            annotation_file_path = os.path.join(annotations_dir, filename)
+            small_df = load_n_adapt_raven_annotation_table_to_dv_dataset_requirements(annotation_file_path,
+                                                                                      annotation_filename_dict)
+            df_list.append(small_df)
+        except UnicodeDecodeError:
+            print('exception:', 'UnicodeDecodeError')
+            print('occured in file:', filename)
+            continue
+        except Exception as e:
+            print('exception:', e)
+            print('occured in file:', filename)
+            continue
+
+    print(len(df_list))
+    df_all_annotations = pd.concat(df_list)
+    df_all_annotations = df_all_annotations.drop(cols2drop, axis=1)
+    # df_all_annotations.head()
