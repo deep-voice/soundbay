089c68f832709b39c10bb5b9e83d1774cd536d88
Noam Bressler <noamzbr@gmail.com>
2023-10-23 08:43:51 +0300

    Initial atmos augments

--- soundbay/custom_audiomentations.py
+++ soundbay/custom_audiomentations.py
@@ -1,14 +1,82 @@
 """soundbay additional custom audio augmentations, based on https://github.com/iver56/audiomentations"""
-
+import os
 import warnings
-import numpy as np
+from functools import lru_cache
 
 from audiomentations import AddBackgroundNoise
+from audiomentations.core.transforms_interface import BaseWaveformTransform
 from audiomentations.core.utils import (
     calculate_desired_noise_rms,
     calculate_rms,
     convert_decibels_to_amplitude_ratio
 )
+
+
+import numpy as np
+import librosa
+from scipy.interpolate import interp1d
+
+
+class AtmosphericFilter(BaseWaveformTransform):
+    """Apply an atmospheric filter to the audio."""
+
+    supports_multichannel = True
+
+    def __init__(
+        self,
+        n_fft=128,
+        p=0.5,
+    ):
+
+        super().__init__(p)
+        self.n_fft = n_fft
+
+    @staticmethod
+    @lru_cache(maxsize=1)
+    def load_filter():
+        base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+        path = os.path.join(base_path, r'quad\filter1.npy')
+        filter_data = np.load(
+            path, allow_pickle=True
+        ).item()
+        return filter_data
+
+    def time_domain_augmentation(self, audio_data, sample_rate):
+        filter_data = self.load_filter()
+
+        audio_data = audio_data.astype(np.float32)
+
+        n_fft = self.n_fft
+        # Compute the short-time Fourier transform
+        y_pad = librosa.util.fix_length(audio_data, size=len(audio_data) + n_fft // 2)
+        Sxx = librosa.stft(y_pad, n_fft=n_fft)
+
+        # Get the frequencies from the STFT
+        frequencies = librosa.fft_frequencies(sr=sample_rate, n_fft=n_fft)
+
+        # Interp filter to match the spectrogram (extrapolate if needed)
+        filter_interp = interp1d(
+            filter_data["freq_vector"][0],
+            filter_data["spl_vector"][0],
+            kind="linear",
+            fill_value="extrapolate",
+        )(frequencies)
+
+        spec_shape = np.shape(Sxx)
+
+        linear_vector = 10 ** (filter_interp / 20)
+
+        filter_interp = np.tile(linear_vector, (spec_shape[1], 1)).T
+
+        Sxx = Sxx * filter_interp
+
+        # Convert back to time domain
+        time_domain_audio = librosa.istft(Sxx, length=len(audio_data))
+
+        return time_domain_audio
+
+    def apply(self, samples: np.array, sample_rate: int):
+        return self.time_domain_augmentation(samples, sample_rate)
 
 
 class AddMultichannelBackgroundNoise(AddBackgroundNoise):
---
+++ tests/test_augs.py
@@ -0,0 +1,37 @@
+import wave
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+
+from soundbay.custom_audiomentations import AtmosphericFilter
+
+
+def test_atmospheric_augmentations():
+    """Test the atmospheric augmentations."""
+    file = r'C:\Users\noam\whale\soundbay\quad\test4.wav'
+    with wave.open(file, "rb") as wav_file:
+        n_frames = wav_file.getnframes()
+        frames = wav_file.readframes(n_frames)
+        audio_data = np.frombuffer(frames, dtype=np.int16)
+        fs = wav_file.getframerate()
+
+    augmented_sig = AtmosphericFilter(n_fft=128, p=1.0)(audio_data, fs)
+
+    # original spectrogram
+    frequencies, times, Sxx = scipy.signal.spectrogram(audio_data, fs)
+    Sxx = 10 * np.log10(Sxx)
+    plt.pcolormesh(Sxx)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
+
+    # augmented spectrogram
+    frequencies, times, Sxx = scipy.signal.spectrogram(augmented_sig, fs)
+    Sxx = 10 * np.log10(Sxx)
+    plt.pcolormesh(Sxx)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
+
+    print()
