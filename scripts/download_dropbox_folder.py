18d89a4dea86018c5b34e98b38c79fe9b2d8c758
mosheman5 <mosheman5@gmail.com>
2024-01-02 16:58:28 +0200

    support download of folders, not only files

--- scripts/download_dropbox_folder.py
+++ scripts/download_dropbox_folder.py
@@ -33,6 +33,37 @@ def upload_to_s3(url, s3_client, user_name, folder_name):
     print("Upload to S3 completed")
 
 
+def recursive_dowload_dropbox_folder_to_s3(dropbox_path, dropbox_handler, s3_handler, user_name, folder_name,
+                                           path_heirarchy=1):
+    """
+    Download a file from Dropbox and upload it to S3
+    """
+    # Iterate through files in the Dropbox folder
+    entries = dropbox_handler.files_list_folder(dropbox_path).entries
+    i = 0
+    for i, entry in tqdm(enumerate(entries)):
+        if isinstance(entry, dropbox.files.FileMetadata):
+            # Generate temporary link for the file
+            print(str(entry) + "\n")
+            link = ""
+            # wrap with try catch to avoid error when there is a download issue
+            try:
+                link = dropbox_handler.files_get_temporary_link(entry.path_display).link
+            except Exception as e:
+                print(f"Error {e}")
+            if link != "":
+                # Upload the file to S3 directly from the link
+                upload_to_s3(link, s3_handler, user_name, folder_name)
+        # In case the entry is a folder, recursively call the function
+        elif isinstance(entry, dropbox.files.FolderMetadata):
+            path_parts = entry.path_display.split("/")[1:]  # remove the first empty string
+            path_parts_to_use = '/'.join(path_parts[path_heirarchy:])
+            recursive_dowload_dropbox_folder_to_s3(entry.path_display, dropbox_handler, s3_handler, user_name,
+                                                   f'{folder_name}/{path_parts_to_use}', path_heirarchy)
+    if i > 0:
+        print(f"Total of {i} files")
+
+
 @click.command()
 @click.option('--dropbox-token', '-d', required=True, type=str,
               help='refer https://app.clickup.com/1861158/v/dc/1rth6-33/1rth6-1282 for details about token generation')
@@ -41,28 +72,25 @@ def upload_to_s3(url, s3_client, user_name, folder_name):
               help='The user name from the dynamodb table, usually the email.')
 @click.option('--folder-name', '-f', required=True, type=str,
               help='The folder name in the user uploads bucket')
-def download_dropbox_folder_to_s3(dropbox_token: str, dropbox_path: str, user_name: str, folder_name: str):
+@click.option('--path-heirarchy', '-h', required=False, type=int, default=1, help='In case the folder name '
+                                                                                  'is not the same as the dropbox path '
+                                                                                  'but a subfolder of it, specify the '
+                                                                                  'path heirarchy level from which we '
+                                                                                  'want to keep the naming in the s3 bucket')
+def download_dropbox_folder_to_s3(dropbox_token: str, dropbox_path: str, user_name: str, folder_name: str,
+                                  path_heirarchy: int = 1):
     # Initialize Dropbox and AWS S3 clients
     dbx = dropbox.Dropbox(dropbox_token)
     s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
-    # Get file metadata from Dropbox folder
-    entries = dbx.files_list_folder(dropbox_path).entries
-    # Iterate through files in the Dropbox folder
-    for i, entry in tqdm(enumerate(entries)):
-        if isinstance(entry, dropbox.files.FileMetadata):
-            # Generate temporary link for the file
-            print(str(entry) + "\n")
-            link = ""
-            # wrap with try catch to avoid error when there is a download issue
-            try:
-                link = dbx.files_get_temporary_link(entry.path_display).link
-            except Exception as e:
-                print(f"Error {e}")
-            if link != "":
-                # Upload the file to S3 directly from the link
-                upload_to_s3(link, s3, user_name, folder_name)
-    print(f"Total of {i} files")
+    # Do the magic
+    recursive_dowload_dropbox_folder_to_s3(dropbox_path=dropbox_path,
+                                           dropbox_handler=dbx,
+                                           s3_handler=s3,
+                                           user_name=user_name,
+                                           folder_name=folder_name,
+                                           path_heirarchy=path_heirarchy)
+
 
 if __name__ == '__main__':
     download_dropbox_folder_to_s3()
