ff63174df2434660d3d459ef50dea04f965e6cc9
mosheman5 <mosheman5@gmail.com>
2024-01-21 21:51:04 +0200

    me being stupid

--- soundbay/utils/checkpoint_utils.py
+++ soundbay/utils/checkpoint_utils.py
@@ -60,6 +60,7 @@ def merge_with_checkpoint(run_args, checkpoint_args):
     run_args.model = OmegaConf.to_container(checkpoint_args.model, resolve=True)
     run_args.data.test_dataset.preprocessors = OmegaConf.to_container(checkpoint_args.data.train_dataset.preprocessors, resolve=True)
     run_args.data.test_dataset.seq_length = checkpoint_args.data.train_dataset.seq_length
+    run_args.data.test_dataset.sample_rate = checkpoint_args.data.train_dataset.sample_rate
     run_args.data.sample_rate = checkpoint_args.data.sample_rate
     run_args.data.n_fft = checkpoint_args.data.n_fft
     run_args.data.hop_length = checkpoint_args.data.hop_length
