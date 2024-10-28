3b0cd9fd7f0cff6120353fcea6c91803384dc6b7
amitgalor18 <amitgalor18@gmail.com>
2024-10-28 20:17:50 +0000

    state from amits machine

--- soundbay/train.py
+++ soundbay/train.py
@@ -77,8 +77,8 @@ def modeling(
     seq_length=train_dataset_args['seq_length'], data_sample_rate=train_dataset_args['data_sample_rate'],
     sample_rate=train_dataset_args['sample_rate'], margin_ratio=train_dataset_args['margin_ratio'],
     slice_flag=train_dataset_args['slice_flag'], mode=train_dataset_args['mode'],
-    path_hierarchy=train_dataset_args['path_hierarchy']
-    )
+    path_hierarchy=train_dataset_args['path_hierarchy'],
+    split_metadata_by_label=train_dataset_args['split_metadata_by_label'])
 
     # train data which is handled as validation data
     train_as_val_dataset = datasets_dict[train_dataset_args['_target_']](data_path=train_dataset_args['data_path'],
@@ -88,8 +88,8 @@ def modeling(
     seq_length=val_dataset_args['seq_length'], data_sample_rate=train_dataset_args['data_sample_rate'],
     sample_rate=train_dataset_args['sample_rate'], margin_ratio=val_dataset_args['margin_ratio'],
     slice_flag=val_dataset_args['slice_flag'], mode=val_dataset_args['mode'],
-    path_hierarchy=val_dataset_args['path_hierarchy']
-    )
+    path_hierarchy=val_dataset_args['path_hierarchy'],
+    split_metadata_by_label=train_dataset_args['split_metadata_by_label'])
 
     val_dataset = datasets_dict[val_dataset_args['_target_']](data_path = val_dataset_args['data_path'],
     metadata_path=val_dataset_args['metadata_path'], augmentations=val_dataset_args['augmentations'],
@@ -98,8 +98,8 @@ def modeling(
     seq_length=val_dataset_args['seq_length'], data_sample_rate=val_dataset_args['data_sample_rate'],
     sample_rate=val_dataset_args['sample_rate'], margin_ratio=val_dataset_args['margin_ratio'],
     slice_flag=val_dataset_args['slice_flag'], mode=val_dataset_args['mode'],
-    path_hierarchy=train_dataset_args['path_hierarchy']
-    )
+    path_hierarchy=train_dataset_args['path_hierarchy'],
+    split_metadata_by_label=val_dataset_args['split_metadata_by_label'])
 
     # Define model and device for training
     model_args = dict(model_args)
