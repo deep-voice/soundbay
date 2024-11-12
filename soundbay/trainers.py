9922ca6b8a8633f52634c380d8ebfdebfcc56c2a
Z30G0D <zeogod@gmail.com>
2024-11-12 21:48:09 +0200

    update wandb experiment ID retrieval in checkpoint saving logic

--- soundbay/trainers.py
+++ soundbay/trainers.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from pathlib import Path
 from soundbay.utils.app import app
 from soundbay.utils.logging import Logger
+import wandb
 
 
 class Trainer:
@@ -174,7 +175,7 @@ class Trainer:
                       "scheduler": self.scheduler.state_dict() if self.scheduler is not None else None,
                       "epochs": self.epochs_trained,
                       "model": self.model.state_dict(),
-                      "wandb_experiment_id": str(self.output_path).split('/')[-1] if not self.debug else None,
+                      "wandb_experiment_id": wandb.run.id if not app.args.experiment.debug else None,
                       "args": app.args
                       }
 
